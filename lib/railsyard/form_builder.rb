cb18b2a67109f9f8f3ff7c8bede2db502c7422af
nebirhos <nebirhos@aol.com>
2012-04-09 02:35:13 +0200

    minor fixes to toggle

--- lib/railsyard/form_builder.rb
+++ lib/railsyard/form_builder.rb
@@ -3,16 +3,16 @@ module Railsyard
   class FormBuilder < SimpleForm::FormBuilder
 
     def group(group_name, options = {}, &block)
-      group_legend = template.link_to translate(:groups, group_name), '#'
+      group_legend = template.link_to translate(:groups, group_name), "##{group_name}"
       group_hint = translate(:group_hints, group_name)
       [
-       template.content_tag( :h3, group_legend, data: {behaviour: "groups-toggle"} ),
+       template.content_tag( :h3, group_legend, id: group_name, data: {behaviour: "groups-toggle"} ),
        template.content_tag( :div, data: {behaviour: "groups-toggle"} ) do
          [
           (template.content_tag(:p, group_hint, class: "hint") if group_hint.present?),
            template.capture(&block)
-           ].join.html_safe
-        end
+         ].join.html_safe
+       end
       ].join.html_safe
     end
 
