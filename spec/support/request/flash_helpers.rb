246853105d5d647a31979bfdfedc681b9dc184c1
Stefano Verna <stefano.verna@welaika.com>
2012-04-12 18:16:07 +0200

    Fixed specs

--- spec/support/request/flash_helpers.rb
+++ spec/support/request/flash_helpers.rb
@@ -2,14 +2,14 @@ module FlashHelpers
 
   def have_notice(message)
     xpath = XPath.generate do |x|
-      x.descendant(:div)[ x.attr(:class).contains('ui-state-notice') & x.contains(message)]
+      x.descendant(:div)[ x.attr(:class).contains('flash-notice') & x.contains(message)]
     end
     Capybara::RSpecMatchers::HaveMatcher.new(:xpath, xpath.to_xpath)
   end
 
   def have_alert(message)
     xpath = XPath.generate do |x|
-      x.descendant(:div)[ x.attr(:class).contains('ui-state-alert') & x.contains(message)]
+      x.descendant(:div)[ x.attr(:class).contains('flash-alert') & x.contains(message)]
     end
     Capybara::RSpecMatchers::HaveMatcher.new(:xpath, xpath.to_xpath)
   end
