ed1ae2a0c468bdec8965ca39cf2979917b20edd5
Stefano Verna <stefano.verna@welaika.com>
2012-05-04 10:16:28 +0200

    Dashboard cannot be accessed without authentication

--- app/controllers/railsyard/static_controller.rb
+++ app/controllers/railsyard/static_controller.rb
@@ -1,4 +1,11 @@
+require 'railsyard-backend/controller'
+
 class Railsyard::StaticController < ApplicationController
+
+  include Railsyard::Backend::Controller::Authentication
+  before_filter :authenticate!
+
   layout "railsyard/admin"
+
   def index; end
 end
