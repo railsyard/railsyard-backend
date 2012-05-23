2c05b738bfab21ba052db9684b39368f6d7e5c86
Stefano Verna <stefano.verna@welaika.com>
2012-05-23 14:17:19 +0200

    Sidebar integration spec

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -7,6 +7,19 @@ feature "Backend" do
     page.should have_content "Dashboard"
   end
 
-  pending "Sidebar is correctly shown"
+  scenario "Sidebar is correctly shown" do
+    # When I visit any page (ie. the Dashboard)
+    visit "/backend"
+
+    within "#page-sidebar nav" do
+      # Then in the siderbar should be present the groups specified in the sidebar.rb config
+      page.should have_xpath ".//h3[text() = 'Resources']"
+      page.should have_xpath ".//h3[text() = 'Settings']"
+
+      # And for each group should be present the links specified
+      page.should have_xpath ".//h3[text() = 'Resources']/following-sibling::ul/li/a[text() = 'Books']"
+      page.should have_xpath ".//h3[text() = 'Settings']/following-sibling::ul/li/a[text() = 'Settings']"
+    end
+  end
 
 end
