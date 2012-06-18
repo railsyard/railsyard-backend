d6efd6fb73f6988a948bb95acb0a6a947b1a7f3e
nebirhos <nebirhos@aol.com>
2012-06-18 17:51:34 +0200

    Fixed name collision on specs

--- spec/generators/railsyard/editor_generator_spec.rb
+++ spec/generators/railsyard/editor_generator_spec.rb
@@ -7,12 +7,12 @@ describe Railsyard::Generators::EditorGenerator do
   destination File.join(root_path, "tmp/spec")
   templates = ActionView::Base.new File.join(root_path, "lib/generators/railsyard/templates")
 
-  arguments %w(Foo title:string:uniq body:text)
+  arguments %w(Narwhal title:string:uniq body:text)
 
   before :all do
     prepare_destination
     run_generator
-    templates.assign(class_name: "Foo",
+    templates.assign(class_name: "Narwhal",
                      fields: [{name: "title", editor_config: "field :title"},
                               {name: "body", editor_config: "field :body"} ])
   end
@@ -21,15 +21,15 @@ describe Railsyard::Generators::EditorGenerator do
 
   it "creates a editor file" do
     editor_content = templates.render(template: 'editor_for_resource.rb.erb')
-    assert_file "app/backend/editor_for_foo.rb", editor_content
+    assert_file "app/backend/editor_for_narwhal.rb", editor_content
   end
 
   it "creates a rails model" do
-    assert_file "app/models/foo.rb"
+    assert_file "app/models/narwhal.rb"
   end
 
   it "creates a rails migration" do
-    assert_migration "db/migrate/create_foos.rb"
+    assert_migration "db/migrate/create_narwhals.rb"
   end
 
 end
